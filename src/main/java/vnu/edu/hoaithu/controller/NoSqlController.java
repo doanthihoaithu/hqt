d9fd32af4a7ab3abbd26d6c2ca43d8beaff708b8
hoaithuxt1998 <hoaithuxt1998@gmail.com>
2018-12-06 17:37:55 +0700

    update

--- src/main/java/vnu/edu/hoaithu/controller/NoSqlController.java
+++ src/main/java/vnu/edu/hoaithu/controller/NoSqlController.java
@@ -16,9 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/nosql")
-@ApiOperation("Truy vấn đối với Oracle NoSQL")
 public class NoSqlController {
-
 
     @Autowired
     private SelectExample selectExample;
@@ -41,19 +39,19 @@ public class NoSqlController {
 
     }
 
-    @PostMapping ("/import")
-    @ApiOperation("Nhập số lượng row muốn import")
-    public ResponseEntity<Long> getImportTime (@RequestParam("begin_id") int beginId, @RequestParam("end_id") int endId) {
-        String storeName = "kvstore";
-        String hostName = "localhost";
-        String hostPort = "5000";
-        KVStoreConfig kvconfig = new KVStoreConfig(storeName, hostName
-                + ":" + hostPort);
-//        kvconfig.setSocketReadTimeout(3600000,TimeUnit.MILLISECONDS);
-        KVStore kvStore = KVStoreFactory.getStore(kvconfig);
-        createExample.init(kvStore);
-        long time = createExample.call(beginId,endId);
-        return new ResponseEntity<Long> (new Long(time), HttpStatus.OK);
-
-    }
+//    @PostMapping ("/import")
+//    @ApiOperation("Nhập số lượng row muốn import")
+//    public ResponseEntity<Long> getImportTime (@RequestParam("begin_id") int beginId, @RequestParam("end_id") int endId) {
+//        String storeName = "kvstore";
+//        String hostName = "localhost";
+//        String hostPort = "5000";
+//        KVStoreConfig kvconfig = new KVStoreConfig(storeName, hostName
+//                + ":" + hostPort);
+////        kvconfig.setSocketReadTimeout(3600000,TimeUnit.MILLISECONDS);
+//        KVStore kvStore = KVStoreFactory.getStore(kvconfig);
+//        createExample.init(kvStore);
+//        long time = createExample.call(beginId,endId);
+//        return new ResponseEntity<Long> (new Long(time), HttpStatus.OK);
+//
+//    }
 }
